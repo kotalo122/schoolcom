1f17397e22b0b46833ad8b87b52375d84355f658
kotalo122 <kotalo122@gmail.com>
2023-05-20 17:36:17 +0900

    ルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -15,6 +15,6 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, :grade_id, user_ids: [])
   end
 end
