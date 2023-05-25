aa24f848e63324cd3f75c02da8262d1df560e3d3
kotalo122 <kotalo122@gmail.com>
2023-05-25 23:13:36 +0900

    イベントメッセージの表示

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,17 @@
 class EventsController < ApplicationController
+  before_action :set_room, only: [:index, :create]
+  
   def index
-    @rooms = Room.all
-    @room = Room.find(params[:room_id])
     @event = Event.new
+    @events = @room.events.includes(:user)
   end
 
   def create
-    @rooms = Room.all
-    @room = Room.find(params[:room_id])
     @event = @room.events.new(event_params)
     if @event.save
       redirect_to room_events_path(@room)
     else
+      @events = @room.events.includes(:user)
       render :index
     end
   end
@@ -22,6 +22,11 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:event_day, :title, :content, :grade_id).merge(user_id: current_user.id)
+  end
+
+  def set_room
+    @rooms = Room.all
+    @room = Room.find(params[:room_id])
   end
 
 end
