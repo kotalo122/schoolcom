c7b55b31e6f71d593c71db201e1f6c2f7adaa93b
kotalo122 <kotalo122@gmail.com>
2023-06-01 20:54:02 +0900

    いいね機能　非同期

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_event
+
   def create
     like = current_user.likes.build(event_id: params[:event_id])
     like.save
-    redirect_to request.referer
+    render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(event_id: params[:event_id], user_id: current_user.id)
     like.destroy
-    redirect_to request.referer
+    render 'destroy.js.erb'
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
+    @room = Room.find(params[:room_id])
   end
 end
