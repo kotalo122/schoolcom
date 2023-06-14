208455e8727048181ceefeba2bc4d639d56f7d7d
kotalo122 <kotalo122@gmail.com>
2023-06-15 00:25:02 +0900

    eventモデルテスト完了

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,18 +1,16 @@
 FactoryBot.define do
-  factory :message do
+  factory :event do
 
-    event_day  { Faker::Date.between(from: Date.today, to: 1.year.from_now) }
-    title      { Faker::Lorem.word }
+    event_day  {Faker::Date.between(from: Date.today, to: 1.year.from_now)}
+    title      {Faker::Lorem.word}
     content    {Faker::Lorem.sentence}
-    grade_id   { Faker::Number.between(from: 2, to: 11) }
-
+    grade_id   {Faker::Number.between(from: 2, to: 11)}
 
     association :user
     association :room
 
     after(:build) do |event|
-      event.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-      event.comments << build(:comment, event: event)
+      event.image.attach(io: File.open('public/images/test_image.png.jpeg'), filename: 'test_image.png')
     end
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @event = FactoryBot.build(:event)
+  end
+
+  describe 'メッセージ投稿' do
+    context 'メッセージが投稿できる場合' do
+      it '日付、title、content、grade_idが存在していれば保存できる' do
+        expect(@event).to be_valid
+      end
+      it 'imageが空でも保存できる' do
+        @event.image = nil
+        expect(@event).to be_valid
+      end
+    end
+    context 'メッセージが投稿できない場合' do
+      it '日付が空では保存できない' do
+        @event.event_day = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Event day can't be blank")
+      end
+      it 'titleが空では保存できない' do
+        @event.title = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'contentが空では保存できない' do
+        @event.content = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Content can't be blank")
+      end
+      it 'grade_idが空では保存できない' do
+        @event.grade_id = ''
+        @event.valid?
+        expect(@event.errors.full_messages).to include("Grade can't be blank")
+      end
+      it "grade_idに「---」が選択されている場合は出品できない" do
+        @event.grade_id = "1"
+        @event.valid?
+        expect(@event.errors.full_messages).to include()
+      end
+      it 'roomが紐付いていないと保存できない' do
+        @event.room = nil
+        @event.valid?
+        expect(@event.errors.full_messages).to include('Room must exist')
+      end
+      it 'userが紐付いていないと保存できない' do
+        @event.user = nil
+        @event.valid?
+        expect(@event.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
